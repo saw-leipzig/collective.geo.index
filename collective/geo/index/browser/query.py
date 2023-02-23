a05f870b76db84cafe6a262c7bd8ad9db617b7a1
muellers-saw-leipzig <muellers@saw-leipzig.de>
2023-02-23 14:13:09 +0100

    updated imports; adding methods to get geo_json string for leaflet

--- collective/geo/index/browser/query.py
+++ collective/geo/index/browser/query.py
@@ -1,10 +1,11 @@
-#
-from zope.interface import Interface, implements
-from zope.publisher.browser import BrowserView
+from zope.interface import Interface, implementer
+from Products.Five.browser import BrowserView
 from Products.CMFCore.utils import getToolByName
-from collective.geo.index.geometryindex import OPERATORS
+from zope.component import queryMultiAdapter
+from .search import get_results
+from shapely import wkt, geometry, box
+import json
 
-from collective.geo.kml.interfaces import IKMLOpenLayersView
 
 class ISearchView(Interface):
     """
@@ -12,9 +13,8 @@ class ISearchView(Interface):
     """
 
 
+@implementer(ISearchView)
 class SearchView(BrowserView):
-
-    implements(ISearchView)
 
     def __init__(self, context, request):
         self.context = context
@@ -24,7 +24,6 @@ class SearchView(BrowserView):
     def portal_catalog(self):
         return getToolByName(self.context, 'portal_catalog')
 
-
     def parse_bbox(self, bbox=None):
         if bbox is None:
             b = self.request.form.get('bbox')
@@ -32,5 +31,72 @@ class SearchView(BrowserView):
             b = bbox
         return tuple(float(x) for x in b.split(','))
 
+    def results(self):
+        results = []
+        brains = get_results(self.context, self.request)
+        for brain in brains:
+            obj = brain.getObject()
+            results.append(obj)
+        return results
 
+    def searchbox_geo_json(self):
+        geo_json = {
+            "type": "FeatureCollection",
+            "features": [],
+        }
+        # bounding box
+        if 'zgeo_geometry' not in self.request:
+            return None
+        bbox = self.request['zgeo_geometry']
+        bbox = self.parse_bbox(bbox)
+        geom = box(*bbox)
 
+        feature = {
+            "type": "Feature",
+            "properties": {
+                "color": "orange",
+                "popup": str(bbox)
+            },
+            "geometry": geometry.mapping(geom)
+        }
+        geo_json['features'].append(feature)
+        return geo_json
+
+    def results_geojson(self):
+        geo_json = {
+            "type": "FeatureCollection",
+            "features": [],
+        }
+        # bounding box
+        if 'zgeo_geometry' not in self.request:
+            return None
+        bbox = self.request['zgeo_geometry']
+        bbox = self.parse_bbox(bbox)
+        geom = box(*bbox)
+        feature = {
+            "type": "Feature",
+            "properties": {
+                "color": "orange",
+                "popup": str(bbox)
+            },
+            "geometry": geometry.mapping(geom)
+        }
+        geo_json['features'].append(feature)
+
+        for geoobj in self.results():
+            geom = wkt.loads(geoobj.geodata)
+            popup_view = queryMultiAdapter(
+                (geoobj, self.request), name="geolocation-geojson-popup"
+            )
+            feature = {
+                "type": "Feature",
+                "properties": {
+                    "popup": popup_view(),
+                    "color": "orange",
+                },
+                "geometry": geometry.mapping(geom)
+            }
+            geo_json['features'].append(feature)
+
+        jsonstring = json.dumps(geo_json)
+        return jsonstring
